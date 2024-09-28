d662ea9577c4b4007647ba5d69b5379a89a9e120
TheuxZn16 <matheusestevam421@gmail.com>
2024-09-27 22:14:25 -0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal('.animation1',{
+    delay: 200,
+    distance: '50px',
+    duration: 1000,
+    origin: 'left',
+    reset: true
+})
+ScrollReveal().reveal('.animation2',{
+    delay: 200,
+    distance: '50px',
+    duration: 1000,
+    origin: 'right',
+    reset: true
+})
+ScrollReveal().reveal('.animation3',{
+    delay: 200,
+    duration: 700,
+    scale: 0.8,
+    easing: 'cubic-bezier(.23,-0.25,.35,1.28)',
+    reset: true
+})
+
